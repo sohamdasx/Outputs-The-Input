f615e93e63d043e06d50f64f2da0f1dfc5b97525
sohamdasx <sohamdas.2702@gmail.com>
2024-06-19 21:07:15 +0530

    done

--- src/Components/InputComponent.js
+++ src/Components/InputComponent.js
@@ -2,19 +2,27 @@ import React, { useState } from "react";
 
 function InputComponent() {
   const [inputValue, setInputValue] = useState("");
+  const [showInputBox, setShowInputBox] = useState(false);
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
 
+  const handleInputBox = () => {
+    setShowInputBox(!showInputBox);
+  };
+
   return (
     <div className="InputContainer">
-      <input
-        type="text"
-        onChange={handleInputChange}
-        placeholder="Type here"
-      ></input>
+      {showInputBox && (
+        <input
+          type="text"
+          onChange={handleInputChange}
+          placeholder="Type here"
+        ></input>
+      )}
       <p className="textOutput">{inputValue}</p>
+      <button onClick={handleInputBox}>Show/Hide</button>
     </div>
   );
 }
